a3bb32cad69efc90922fb7570a6cc2559d964296
Your Name~ <you@example.com>
2023-05-30 10:54:01 +0300

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,46 @@
+import './libs/jquery-3.5.1.min.js'
+
+
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
-  
+    function headerfixed() {
+        const header = $(".header-fixed-js");
+        let scrollChange = 1;
+
+        $(window).scroll(function () {
+            let scroll = $(window).scrollTop();
+
+            if (scroll >= scrollChange) {
+                header.addClass('header-fixed');
+            } else {
+                header.removeClass("header-fixed");
+            }
+        });
+    }
+
+    headerfixed();
+
+
+     function burger() {
+            const js__headerBurgerClick = document.querySelectorAll('.js__header-burgerclick')
+            const js__headerBurger = document.querySelectorAll('.js__header-burger');
+            const headerNavOpenJs = document.querySelector('.header-nav-open-js');
+            const body = document.querySelector('body');
+        
+            
+            for (let i = 0; i < js__headerBurgerClick.length; i++) {
+                js__headerBurgerClick[i].addEventListener('click', () => {   
+                    for (let i = 0; i < 3; i++)  {
+                        js__headerBurger[i].classList.toggle('open');
+                        headerNavOpenJs.classList.toggle('open');
+                        body.classList.toggle('lock');
+                    }
+                });
+            }
+     }
+        
+    burger();
 }
 
 
