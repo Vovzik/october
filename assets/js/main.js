99310cfe5d4c85f91e0ed5207dfa6ed963c6a735
Your Name~ <you@example.com>
2023-05-30 17:17:41 +0300

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 import './libs/jquery-3.5.1.min.js'
+import './libs/jquery.fancybox.min.js'
+import './libs/swiper-bundle.min.js'
 
 
 document.addEventListener("DOMContentLoaded", ready);
@@ -41,6 +43,114 @@ function ready() {
      }
         
     burger();
+
+
+    function popup() {
+      const js__popupLink = document.querySelectorAll('.js__popup-link');
+      const js__popupClose = document.querySelectorAll('.js__popup-close');
+      const body = document.querySelector('body');
+      const popupVideoJs = document.querySelector('.popup-video-js');
+    
+    
+      let unlock = true;
+    
+      const timeout = 400;
+    
+    
+    
+    
+      for (let i = 0; i < js__popupLink.length; i++) {
+        js__popupLink[i].addEventListener('click', (event) => {
+          const popup__name = js__popupLink[i].dataset.modal;
+          const popupCurent = document.querySelector(`[data-popup="${popup__name}"]`);
+          popupOpen(popupCurent);
+          event.preventDefault();
+        });
+      };
+    
+    
+      for (let i = 0; i < js__popupClose.length; i++) {
+        js__popupClose[i].addEventListener('click', (event) => {
+          popupClose(js__popupClose[i].closest('.js__popup-open'));
+          event.preventDefault();
+        });
+      };
+    
+    
+    
+      function popupOpen(popupCurent) {
+        if (popupCurent && unlock) {
+          const popupActive = document.querySelector('.js__popup-open.open');
+    
+          if (popupActive) {
+            popupClose(popupActive, false);
+          } else {
+            bodylock();
+          }
+    
+    
+          popupCurent.classList.add('open');
+          popupCurent.addEventListener('click', (event) => {
+            if (!event.target.closest('.js__popup-content')) {
+              popupClose(event.target.closest('.js__popup-open'));
+              console.log(true);
+            } else {
+              console.log(false);
+            }
+          });
+        };
+      }
+    
+    
+      function popupClose(popupActive, doUnlock = true) {
+        if (unlock) {
+          popupActive.classList.remove('open');
+          popupVideoJs.pause();
+          if (doUnlock) {
+            bodyUnlock();
+          };
+        };
+      };
+    
+    
+      function bodylock() {
+        const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px'; //Получили ширину scrolla
+    
+        body.style.paddingRight = lockPaddingValue;
+        body.classList.add('lock');
+    
+      };
+    
+    
+      function bodyUnlock() {
+        setTimeout(function () {
+          body.style.paddingRight = '0px';
+          body.classList.remove('lock');
+        }, timeout)
+      };
+    }
+    
+    popup();
+
+    function gallerySlider () {
+      new Swiper('.gallery-slider-js', {
+       // Optional parameters
+       slidesPerView: 1,
+       loop: true,
+       // Navigation arrows
+       navigation: {
+         nextEl: '.gallery-next-js',
+         prevEl: '.gallery-prev-js',
+       },
+       effect: 'fade',
+       fadeEffect: {
+         crossFade: true
+       },
+     
+     });
+    }
+
+    gallerySlider();
 }
 
 
